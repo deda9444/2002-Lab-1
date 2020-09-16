9b984f030c23ea2dac5231392572309044820d06
Lucas <Lucas@DESKTOP-K5A5DEH>
2020-09-16 16:30:08 -0600

    In depth comments

--- MainCode.m
+++ MainCode.m
@@ -6,23 +6,33 @@ clc;
 
 %Variables
 
-R_h = 8.24978430917; 
-rho_mat = 1500;
-p_gage = 10;
-k = 500;
-%Equations
-rho_h = pres/(temp*R_h)/1000;
-ys = 9*10^6; %[MPa]
-FoS = 1.5;
+R_h = 8.24978430917; % universal gas constant divided by molar mass of H = 1.00784 (Pa*m^3/(g*K))
+rho_mat = 1500; % Density of LLDPE (kg/m^3)
+p_gage = 10; % Assumed gage pressure inside balloon (Pa)
+k = 500; % Mass of payload (kg)
+rho_h = pres/(temp*R_h)/1000; % Density of H based on atmospheric conditions and P = pRT (kg/m^3)
+ys = 9*10^6; % Yield strength of LLDPE (Pa)
+FoS = 1.5; % Factor of safety
+
+
 %Equations
 
+% Equation for radius of balloon, set by equating mass to buoyant force and
+% substituting wherever possible for radius, then solving
 r = (((6 / pi) ^ (1 / 3) * ys * (k ^ (1/3))) / ((8 * (ys ^ 3) * (rho - rho_h)) - ((rho_mat * p_gage * FoS) * ((p_gage ^ 2) * (FoS ^ 2) + (6 * p_gage *FoS * ys) + (12 * (ys ^ 2))))) ^ (1 / 3));
 
+% Volume of balloon interior
 v = 4/3 * pi * r^3;
+% thickness of balloon skin
 thickness = r*FoS*p_gage/(2*ys);
+% mass of balloon material
 m_mat = (4/3 * pi * (thickness + r)^3 - v)*rho_mat;
+% mass of hydrogen
 m_h = v * rho_h;
+% total mass of system
 m = k + m_mat + m_h;
+% Buoyant force on balloon
 Fb = rho*9.8065*v;
+% Force of gravity on system
 Fg = m * 9.8065;
 
